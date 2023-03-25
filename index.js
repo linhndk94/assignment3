a206a454a93eff9b1ab6a3e1558d7b6fdac4f12e
KhanhLinh <linhndk94@gmail.com>
2023-03-26 04:24:28 +0700

    add s3

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const amqplib = require('amqplib');
 
 const amqp_url = "amqp://assignment3:assignment3@54.237.122.168:5672";
 
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
+
+const region = "us-east-1";
+const s3Client = new S3Client({region: region});
+const bucket = "assignment3-user2386042";
+
 async function consume() {
     const conn = await amqplib.connect(amqp_url, "heartbeat=60");
     const ch = await conn.createChannel()
     const q = 'assignment3_image_queue';
-    await ch.consume(q, function (msg) {
+    await ch.consume(q, async function (msg) {
         console.log(msg.content.toString());
         ch.ack(msg);
-        // ch.cancel('image_consumer');
+        const input = {
+            "Bucket": bucket,
+            "Key": msg
+        }
+        const command = new GetObjectCommand(input);
+        const response = await s3Client.send(command);
+        console.log(JSON.stringify(response));
     }, {consumerTag: 'image_consumer'});
 }
 
