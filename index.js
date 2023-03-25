3aab3eb1b2f78741e23273c21c02521d1ff75ae2
KhanhLinh <linhndk94@gmail.com>
2023-03-26 04:35:36 +0700

    fix json

--- index.js
+++ index.js
@@ -21,7 +21,18 @@ async function consume() {
         }
         const command = new GetObjectCommand(input);
         const response = await s3Client.send(command);
-        console.log(JSON.stringify(response));
+        let cache = [];
+        JSON.stringify(response, (key, value) => {
+            if (typeof value === 'object' && value !== null) {
+                // Duplicate reference found, discard key
+                if (cache.includes(value)) return;
+
+                // Store value in our collection
+                cache.push(value);
+            }
+            return value;
+        });
+        cache = null; // Enable garbage collection
     }, {consumerTag: 'image_consumer'});
 }
 
